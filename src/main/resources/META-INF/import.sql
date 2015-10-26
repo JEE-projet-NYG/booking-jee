647764f1a6079c08982834db555b3627d6644c35
TidyMaze <GrosKiff@gmail.com>
2015-10-26 21:09:56 +0100

    Données démo

--- src/main/resources/META-INF/import.sql
+++ src/main/resources/META-INF/import.sql
@@ -10,12 +10,21 @@ INSERT INTO User (id, admin, firstname, lastname, login, mail_address, password,
 INSERT INTO User (id, admin, firstname, lastname, login, mail_address, password, phone_number) VALUES (5, 0, 'Arya', 'Stark', 'astark', 'astark@yopmail.fr', 'astark', '0302010203');
 
 -- RessourceType
-INSERT INTO RESOURCETYPE (id, name) VALUES (1,'MEUBLE');
-INSERT INTO RESOURCETYPE (id, name) VALUES (2,'OUTILLAGE');
+INSERT INTO RESOURCETYPE (id, name) VALUES (1,'Mobilier');
+INSERT INTO RESOURCETYPE (id, name) VALUES (2,'Outillage');
+INSERT INTO RESOURCETYPE (id, name) VALUES (3,'Transport');
 
 -- Ressources
-INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('1', 'table', 'ceci est une table magnifique', 'salle B116', 1, 1);
+INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('1', 'Table', 'Petite table d''intérieur', 'Salon', 1, 1);
+INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('2', 'Table IKEA', 'Petite table difficile à construire', 'Bureau', 2, 1);
+INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('3', 'Tournevis sonique', 'Permet d''ouvrir toutes les portes', 'TARDIS', 3, 2);
+INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('4', 'Tournevis laser', 'Permet d''ouvrir toutes les portes blindées', 'TARDIS', 3, 2);
+INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('5', 'Voiture UBER', 'Expérimentez l''illégalité', 'Nantes', 1, 3);
+INSERT INTO RESOURCE (id, name, description, localisation, responsibleId, typeId) VALUES ('6', 'Avion privé', 'La vitesse au maximum', 'Paris', 4, 3);
 
 -- Réservations
-INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (1, PARSEDATETIME('2015/10/22 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2015/10/30 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 5, 1);
-INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (2, PARSEDATETIME('2015/09/20 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2015/11/05 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 2, 1);
+INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (1, PARSEDATETIME('2015/10/22 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2015/10/30 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 1, 1);
+INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (2, PARSEDATETIME('2015/11/01 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2015/12/10 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 2, 1);
+INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (3, PARSEDATETIME('2016/01/01 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2016/01/10 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 3, 1);
+INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (4, PARSEDATETIME('2015/10/26 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2015/11/05 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 3, 3);
+INSERT INTO Reservation (id, date_start, date_end, borrowerId, resourceId) VALUES (5, PARSEDATETIME('2015/11/05 12:05:00', 'yyyy/MM/dd HH:mm:ss'), PARSEDATETIME('2016/01/10 12:05:00', 'yyyy/MM/dd HH:mm:ss'), 3, 4);
